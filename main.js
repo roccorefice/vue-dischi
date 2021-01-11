0cc8ddde022a14e1c6634828533deb3e476cf9ad
Rocco Orefice <rocco.orefice92@gmail.com>
2021-01-11 15:47:17 +0100

    primo commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Istruzioni:
+// Attraverso una chiamata ajax all’Api di boolean avremo a disposizione una decina di dischi musicali. 
+// Servendoci di Vue JS stampiamo tutto a schermo.
+// In questo momento non è importante la parte grafica.
+// Bonus: Creare una select con i seguenti generi: pop, rock, metal e jazz. In base a cosa scegliamo nella select vedremo i corrispondenti cd.
+// Chiamata: https://flynn.boolean.careers/exercises/api/array/music
+// Layout base:
+// https://bitbucket.org/booleancareers/ex-dischi-musicali-layout
+
+let app = new Vue ({
+	el:"#app",
+	data:{
+		dischi:[],
+	},
+	mounted(){
+		axios
+		.get('https://flynn.boolean.careers/exercises/api/array/music')
+		.then(response => {
+			dischi =  response.data.response;
+			// this.dischi.push(dischi);
+			console.log(dischi);
+		});
+	}
+})
